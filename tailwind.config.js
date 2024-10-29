131816e39091564a67025e7df173521bba50293f
Asliddin <asliddinnorboyev2401@gmail.com>
2024-10-29 14:16:20 +0300

    the second lesson is working

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        "main_container":"1240px"
+      },
+      fontWeight: {
+        "400":"400",
+        "500":"500",
+        "600":"600",
+        "700":"700"
+      }
+    },
   },
   plugins: [],
 }
